7472d2e801be564d8f3d398001d666b720c6cfea
Seth <hullogh@gmail.com>
2023-09-19 13:28:11 +0800

    fix(web): feature deck button missing transition

--- packages/web/components/features.tsx
+++ packages/web/components/features.tsx
@@ -8,15 +8,29 @@ export default function Features() {
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6 ">
         <div className="pb-8 md:pb-10">
-          <HeroImage
-            src={heroDashboardImage}
-            alt="Dashboard of the Purpose Bound Money (PBM) app"
-          />
+          <div
+            className="relative flex justify-center items-center min-w-[60rem] sm:min-w-full"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <HeroImage
+              src={heroDashboardImage}
+              alt="Dashboard of the Purpose Bound Money (PBM) app"
+            />
+          </div>
         </div>
         <div className="flex justify-center pb-10 md:pb-16">
-          <SecondaryLinkButton href={process.env.NEXT_PUBLIC_UX_FEATURE_DECK_URL!}>
-            See Feature Screens Deck
-          </SecondaryLinkButton>
+          <div
+            className="relative flex justify-center items-center min-w-[60rem] sm:min-w-full"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <SecondaryLinkButton
+              href={process.env.NEXT_PUBLIC_UX_FEATURE_DECK_URL!}
+            >
+              See Feature Screens Deck
+            </SecondaryLinkButton>
+          </div>
         </div>
       </div>
     </section>
--- packages/web/components/hero-image.tsx
+++ packages/web/components/hero-image.tsx
@@ -11,20 +11,12 @@ interface HeroImageProps {
 export default function HeroImage({ src, alt }: HeroImageProps) {
   return (
     <div>
-      <div>
-        <div
-          className="relative flex justify-center items-center min-w-[60rem] sm:min-w-full"
-          data-aos="fade-up"
-          data-aos-delay="200"
-        >
-          <Image
-            src={src}
-            alt={alt}
-            placeholder="blur"
-            className="drop-shadow-xl sm:drop-shadow-2xl rounded-md"
-          />
-        </div>
-      </div>
+      <Image
+        src={src}
+        alt={alt}
+        placeholder="blur"
+        className="drop-shadow-xl sm:drop-shadow-2xl rounded-md"
+      />
     </div>
   );
 }
