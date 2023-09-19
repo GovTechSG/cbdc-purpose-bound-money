cc9f538ecc3994c031e1e17ef10e894db4b5dd9b
Seth <hullogh@gmail.com>
2023-09-19 15:09:03 +0800

    fix(web): footer text size

--- packages/web/components/ui/footer.tsx
+++ packages/web/components/ui/footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
       <div className="py-12 md:py-16">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="border-t border-t-gray-100 pt-16 min-h-[5] drop-shadow-lg" />
-          <div className="text-center text-gray-400 text-sm leading-5">
+          <div className="text-center text-gray-400 text-xs md:text-sm leading-5">
             <div>Built with ❤️ by GovTech for Project Orchid.</div>
-            <div>&copy; 2023 GovTech. All rights reserved.</div>
+            <div>&copy; 2023. All rights reserved.</div>
           </div>
         </div>
       </div>
