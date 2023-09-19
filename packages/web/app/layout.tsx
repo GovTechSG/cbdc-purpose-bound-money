ef65b0ca85fb3c065f9a236ad7c7625f078beea8
Seth <hullogh@gmail.com>
2023-09-19 21:40:28 +0800

    feat(web): add favicons
    feat(app): add favicons

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -28,6 +28,12 @@ export const metadata: Metadata = {
     minimumScale: 1,
   },
   themeColor: "#2563EB",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
