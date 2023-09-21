82ea2cb942859499cd88e369003827e075d410de
Seth <hullogh@gmail.com>
2023-09-22 02:52:15 +0800

    chore(app): update chain addresses

--- packages/app/src/common/config/app-chain-info.ts
+++ packages/app/src/common/config/app-chain-info.ts
@@ -10,10 +10,13 @@ export const chainInfoPolygon: AppChainInfo = {
 
 export const chainInfoMumbai: AppChainInfo = {
     pbm: {
-        address: '0x56e932991885B65bc91f719d9dac241E64B6529E',
-        genesisBlock: 39239804,
+        address: '0x27fFde7Eeb50d5a7E0288B6142377d0fD7FEA115',
+        genesisBlock: 40360319,
     },
-    payees: ['0x24ba19062400185a9001EDC9F6200C438F62Fa53'],
+    payees: [
+        '0x7bd37875BBC40b43Fd6557D6542b5A1b528cD9f4',
+        '0xa50B92dA032a0E150dabc8FD4b50C5D1ef7ef94F',
+    ],
 }
 
 export const chainInfoLocal: AppChainInfo = {
