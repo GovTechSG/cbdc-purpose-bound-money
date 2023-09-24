4ee165aa1b32fe62a66ab7b81fea526f8d4a2231
Seth <hullogh@gmail.com>
2023-09-24 18:51:33 +0800

    feat(app): add responsive wallet holding stats

--- packages/app/src/components/token-overview/pbm-overview-card/index.tsx
+++ packages/app/src/components/token-overview/pbm-overview-card/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { Col, Row, Space } from 'antd'
-import { useTokenBalance } from '@app/hooks/use-token-balance'
-import { useGetWithdrawalBalance } from '@app/hooks/use-get-withdrawal-balance'
-import { usePBMTokenContext } from '@app/contexts/pbm-token-context'
-import { BigNumber } from 'ethers'
-import { WithdrawalBalanceStatistic } from '@app/components/token-overview/pbm-overview-card/withdrawal-balance-statistic'
-import { LockedBalanceStatistic } from '@app/components/token-overview/pbm-overview-card/locked-balance-statistic'
-import { useIsMounted } from '@app/hooks/use-is-mounted'
-import { TokenAllocationChart } from '@app/components/token-overview/pbm-overview-card/token-allocation-chart'
+import { AddressBookLabel } from '@app/components/address-book-label'
+import { AddressTooltip } from '@app/components/address-tooltip'
+import { SkeletonLoader } from '@app/components/skeleton-loader'
 import { DashboardCard } from '@app/components/token-overview/dashboard-card'
 import { AllocationLegendIndicator } from '@app/components/token-overview/pbm-overview-card/allocation-legend-indicator'
-import { SkeletonLoader } from '@app/components/skeleton-loader'
-import { AddressTooltip } from '@app/components/address-tooltip'
-import { AddressBookLabel } from '@app/components/address-book-label'
+import { TokenBalanceStatistic } from '@app/components/token-overview/pbm-overview-card/locked-balance-statistic'
+import { TokenAllocationChart } from '@app/components/token-overview/pbm-overview-card/token-allocation-chart'
+import { WithdrawalBalanceStatistic } from '@app/components/token-overview/pbm-overview-card/withdrawal-balance-statistic'
+import { usePBMTokenContext } from '@app/contexts/pbm-token-context'
+import { useGetWithdrawalBalance } from '@app/hooks/use-get-withdrawal-balance'
+import { useIsMounted } from '@app/hooks/use-is-mounted'
+import { useTokenBalance } from '@app/hooks/use-token-balance'
+import { Col, Grid, Row, Space } from 'antd'
+import { BigNumber } from 'ethers'
+import { formatUnits } from 'ethers/lib/utils'
+import React from 'react'
 
 interface WithdrawalStatisticProps {
     connectedAddress: string | `0x${string}` | undefined
@@ -54,11 +55,14 @@ export const PbmOverviewCard: React.FC<WithdrawalStatisticProps> = ({
     connectedAddress,
     pbmBalance,
 }) => {
+    const screens = Grid.useBreakpoint()
+
     const {
         decimals: pbmDecimals,
         contract: pbmContract,
         symbol: pbmSymbol,
         vaultContract: pbmVaultContract,
+        loading: pbmContextLoading,
     } = usePBMTokenContext()
 
     const {
@@ -79,7 +83,7 @@ export const PbmOverviewCard: React.FC<WithdrawalStatisticProps> = ({
     return (
         <DashboardCard title={<CardTitle connectedAddress={connectedAddress} />}>
             <Row gutter={16}>
-                <Col span={5}>
+                <Col span={5} xxl={4}>
                     <Space direction="vertical" align="center">
                         <TokenAllocationChart
                             withdrawalBalance={withdrawalBalance}
@@ -89,13 +93,15 @@ export const PbmOverviewCard: React.FC<WithdrawalStatisticProps> = ({
                             pbmBalance={pbmBalance}
                             colours={legendColours}
                         />
-                        <AllocationLegendIndicator
-                            colour={legendColours.holding}
-                            title="Wallet Holding"
-                        />
+                        {!screens.xxl && (
+                            <AllocationLegendIndicator
+                                colour={legendColours.holding}
+                                title="Wallet Holding"
+                            />
+                        )}
                     </Space>
                 </Col>
-                <Col span={9} offset={1}>
+                <Col span={9} xxl={6} offset={1}>
                     <WithdrawalBalanceStatistic
                         withdrawalBalance={withdrawalBalance}
                         withdrawableDepositIds={withdrawableDepositIds}
@@ -106,12 +112,22 @@ export const PbmOverviewCard: React.FC<WithdrawalStatisticProps> = ({
                         pbmSymbol={pbmSymbol}
                     />
                 </Col>
-                <Col span={9}>
-                    <LockedBalanceStatistic
-                        lockedDecimalBalance={vPbmDecimalBalance}
+                <Col span={9} xxl={{ span: 6, offset: 1 }}>
+                    <TokenBalanceStatistic
+                        title={'Pending Withdrawal'}
+                        decimalBalance={vPbmDecimalBalance}
                         loading={vPbmLoading}
                         pbmSymbol={pbmSymbol}
                         legendColour={legendColours.pending}
+                    />
+                </Col>
+                <Col span={0} xxl={6}>
+                    <TokenBalanceStatistic
+                        title={'Wallet Holding'}
+                        decimalBalance={Number(formatUnits(pbmBalance, pbmDecimals))}
+                        loading={pbmContextLoading}
+                        pbmSymbol={pbmSymbol}
+                        legendColour={legendColours.holding}
                     />
                 </Col>
             </Row>
--- packages/app/src/components/token-overview/pbm-overview-card/locked-balance-statistic/index.tsx
+++ packages/app/src/components/token-overview/pbm-overview-card/locked-balance-statistic/index.tsx
@@ -1,27 +1,29 @@
-import React from 'react'
 import { StatisticTitleWithLoader } from '@app/components/token-overview/pbm-overview-card/statistic-title-with-loader'
+import React from 'react'
 
-interface LockedBalanceStatisticProps {
-    lockedDecimalBalance: string | undefined
+interface TokenBalanceStatisticProps {
+    title: string
+    decimalBalance: string | number | undefined
     loading: boolean
     pbmSymbol: string
     legendColour: `#${string}`
 }
 
-export const LockedBalanceStatistic: React.FC<LockedBalanceStatisticProps> = ({
-    lockedDecimalBalance,
+export const TokenBalanceStatistic: React.FC<TokenBalanceStatisticProps> = ({
+    title,
+    decimalBalance,
     loading,
     pbmSymbol,
     legendColour,
 }) => {
-    const title = `Pending Withdrawal (${pbmSymbol})`
+    const titleWithSymbol = `${title} (${pbmSymbol})`
 
     return (
         <StatisticTitleWithLoader
-            title={title}
+            title={titleWithSymbol}
             legendColour={legendColour}
             loading={loading}
-            value={lockedDecimalBalance}
+            value={decimalBalance}
         />
     )
 }
