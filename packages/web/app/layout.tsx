7f2f32e6f9c847928de73e13014f0e11be8a8522
Seth <hullogh@gmail.com>
2023-11-16 20:54:45 +0800

    refactor(web): move og into metadata exports

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -3,6 +3,7 @@ import Footer from "@web/components/ui/footer";
 import Header from "@web/components/ui/header";
 import { Metadata } from "next";
 import { Inter, Architects_Daughter } from "next/font/google";
+import Head from "next/head";
 import Script from "next/script";
 
 const inter = Inter({
@@ -36,6 +37,20 @@ export const metadata: Metadata = {
   },
   manifest: "/site.webmanifest",
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL!),
+  openGraph: {
+    type: "website",
+    title: "The Purpose Bound Money (PBM)",
+    description:
+      "A protocol, developed as part of Project Orchid, enabling the use of digital money with automated on-chain escrow payment releases.",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Purpose Bound Money (PBM)",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -48,6 +63,9 @@ export default function RootLayout({
   // noinspection HtmlRequiredTitleElement
   return (
     <html lang="en">
+      <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body
         className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-white text-gray-700 tracking-tight bg-top bg-no-repeat bg-auto bg-blend-multiply bg-page`}
       >
